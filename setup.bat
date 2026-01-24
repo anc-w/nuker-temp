a9ed3dd10d5ea1dfbd8fde6e8bb79a6009e896e9
ANC <138563451+anc-w@users.noreply.github.com>
2026-01-24 13:22:53 -0500

    Add setup script for environment installation

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+color a
+cls
+echo ANC Nuker temp, edit variables and names in main.py, fully open source
+echo Installing requirements...
+pip install -r requirements.txt
+echo Done! any key to start!
+pause
+python main.py
